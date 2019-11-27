361a87f6da054fc00945d36c07292ff441331728
Guillaume Lostis <g.lostis@kayrros.com>
2019-11-27 19:00:37 +0100

    Add docstrings to test script

--- tests/data/script_to_lint.py
+++ tests/data/script_to_lint.py
@@ -1,5 +1,11 @@
+"""This module contains sample code to test how pylint reacts to it
+"""
 from elasticsearch_dsl import Q
 
 
-def main():
+def unary_operator_on_q():
+    """This function applies the ~ operator to a Q object
+
+    This normally raises a pylint error
+    """
     return ~Q("exists", field="foo")
